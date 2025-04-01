88c947e342aeab8fabd342aa209d9036f36a999e
Ryan Hoang <anhoang800140@gmail.com>
2025-04-01 17:46:19 +0700

    selector with filter child

--- JQuery/Import/js/main.js
+++ JQuery/Import/js/main.js
@@ -38,17 +38,25 @@
 //   $("a[href]").css("color", "blue"); // Selects all links with href and changes their color to blue
 //   $("a[href *= 'product'][title *= ' ']").css("color", "gray"); // Selects all links with href containing 'Product' and title containing ' ' and changes their color to gray , selector multi attribute
 // });
+// $(function () {
+//   $("p:first").css("color", "red"); // selector mount to index first
+//   $("p:last").css("color", "blue"); // selector mount to index last
+//   $("p:eq(2)").css("color", "green"); // selector mount to index 2
+//   $("p:lt(3)").css("color", "darkBlue"); // selector mount to index less than 3
+//   $("p:gt(3)").css("color", "darkRed"); // selector mount to index greater than 3
+//   $("p:even").css("color", "purple"); // selector mount to index even
+//   $("p:odd").css("color", "orange"); // selector mount to index odd
+//   $("p:contains('1')").css("color", "yellow"); // selector mount to index contains 1
+//   $("p:not('p.p2')").css("color", "blue"); // selector mount to index not 1
+//   //   $("p:has(span)").css("color", "pink"); // selector mount to index has span
+//   //   $("p:parent").css("color", "pink"); // selector mount to index parent
+//   //   $("p:empty").css("color", "pink"); // selector mount to index empty
+// });
 $(function () {
-  $("p:first").css("color", "red"); // selector mount to index first
-  $("p:last").css("color", "blue"); // selector mount to index last
-  $("p:eq(2)").css("color", "green"); // selector mount to index 2
-  $("p:lt(3)").css("color", "darkBlue"); // selector mount to index less than 3
-  $("p:gt(3)").css("color", "darkRed"); // selector mount to index greater than 3
-  $("p:even").css("color", "purple"); // selector mount to index even
-  $("p:odd").css("color", "orange"); // selector mount to index odd
-  $("p:contains('1')").css("color", "yellow"); // selector mount to index contains 1
-  $("p:not('p.p2')").css("color", "blue"); // selector mount to index not 1
-  //   $("p:has(span)").css("color", "pink"); // selector mount to index has span
-  //   $("p:parent").css("color", "pink"); // selector mount to index parent
-  //   $("p:empty").css("color", "pink"); // selector mount to index empty
+  $("li:first-child").css("color", "red"); //select first child
+  $("li:last-child").css("color", "blue"); //select last child
+  $("li:nth-child(2)").css("color", "green"); //select nth child
+  $("li:nth-child(3n+1)").css("color", "darkBlue"); //select nth with operation child
+  $("li:only-child").css("color", "darkRed"); //select only child
+  $("li").css("color", "gray"); //select nth child
 });
