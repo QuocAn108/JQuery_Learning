691b825250201e049ab9ff82707e1d8c82c77cfa
Ryan Hoang <anhoang800140@gmail.com>
2025-04-01 13:12:44 +0700

    exercise change color when click btn

--- JQuery/Import/js/main.js
+++ JQuery/Import/js/main.js
@@ -1,11 +1,15 @@
-$(document).ready(function () {
-  // This function runs when the DOM is fully loaded
-  $("body").append("<h1>Hello World</h1>");
-});
+// $(function () {
+//   $("h1 .hihi").text("Hoang Quoc An"); // Change the text of the h1 element, h1 is the selector
+//   //h1 is the tag name, .hihi is the class name.
+//   //text() is a method that sets or returns the text content of the selected elements.
+//   $("h1").css("color", "red");
+// });
 
 $(function () {
-  $("h1 .hihi").text("Hoang Quoc An"); // Change the text of the h1 element, h1 is the selector
-  //h1 is the tag name, .hihi is the class name.
-  //text() is a method that sets or returns the text content of the selected elements.
-  $("h1").css("color", "red");
+  $(".btn-red").click(function () {
+    $("p.name").css("color", "red");
+  });
+  $(".btn-blue").click(function () {
+    $("p.name").css("color", "blue");
+  });
 });
